f8e43114ae599ca341eb23a27f8ab8f35a5d40da
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-11-20 16:46:18 -0500

    clear

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BlackJack blackjack = new BlackJack();
+        BlackJack BlackJack = new BlackJack();
     }
 }
