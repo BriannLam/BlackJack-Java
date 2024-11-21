624118223969cf758ec7d6d8ed1ae14b9748c9be
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-11-21 15:51:35 -0500

    put in-progress code into BrianJack

--- src/BrianJack.java
+++ src/BrianJack.java
@@ -1,3 +1,186 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
 public class BrianJack {
+
+    private class Card{
+        String value; 
+        String type; 
+
+        Card(String value, String type){
+            this.value = value;
+            this.type = type;
+        }
+
+        public String toString(){
+            return value + "-" + type;
+        }
+
+        public int getValue(){
+            if("AJQK".contains(value)){ // A J O K
+                if(value == "A"){
+                    return 11;
+                }
+                return 10;
+            }
+            return Integer.parseInt(value); //2-10
+        }
+        
+        
+        public boolean isAce(){
+            return value == "A";
+        }
+    }
+
+    ArrayList<Card> deck;
+    Random random = new Random(); //shuffling the deck 
+
+
+    //dealer 
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum; 
+    int dealerAceCount;
+
+    //player 
+    ArrayList<Card> playerHand;
+    int playerSum;
+    int playerAceCount;
     
+    //window 
+    int boardWidth = 600;
+    int boardHeight = boardWidth;
+
+    int cardWidth = 110;
+    int cardHeight = 154; //changable ration should be 1:1.4
+
+    JFrame frame = new JFrame("Black Jack");
+    JPanel gamePanel = new JPanel() {
+        @Override
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+
+            try {
+                Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
+                g.drawImage(hiddenCardImg, 20, 20, cardWidth, cardHeight, null);
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+        }
+    };
+    JPanel buttonPanel = new JPanel();
+    JButton hitButton = new JButton("Hit");
+    JButton stayButton = new JButton("Stay");
+
+
+
+    BrianJack() {
+        startGame();
+
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gamePanel.setLayout(new BorderLayout());
+        gamePanel.setBackground(new Color(53, 101, 77));
+        frame.add(gamePanel);
+
+        hitButton.setFocusable(false);
+        buttonPanel.add(hitButton);
+        stayButton.setFocusable(false);
+        buttonPanel.add(stayButton);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+    }
+
+    public void startGame(){
+        //deck 
+        buildDeck();
+        shuffleDeck();
+        
+        
+        //dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount = 0;
+
+        hiddenCard = deck.remove(deck.size()-1); //remove card at last index 
+        dealerSum += hiddenCard.getValue();
+        dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+        Card card = deck.remove(deck.size()-1);
+        dealerSum += card.getValue();
+        dealerAceCount += card.isAce() ? 1: 0;
+        dealerHand.add(card);
+
+        System.out.println("DEALER:");
+        System.out.println(hiddenCard);
+        System.out.println(dealerHand);
+        System.out.println(dealerSum);
+        System.out.println(dealerAceCount);
+
+        //player 
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
+
+        for(int i = 0; i<2; i++){
+            card = deck.remove(deck.size()-1);
+            playerSum += card.getValue();
+            playerAceCount += card.isAce() ? 1 : 0;
+            playerHand.add(card);
+        }
+
+
+        System.out.println("PLAYER:");
+        System.out.println(playerHand);
+        System.out.println(playerSum);
+        System.out.println(playerAceCount);
+
+    }
+
+
+    public void buildDeck(){
+        deck = new ArrayList<Card>();
+        String[] value = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        String[] types = {"C", "D", "H", "S"};
+
+        for( int i = 0; i <types.length; i++){
+            for(int j = 0; j < value.length; j++){
+                Card card = new Card(value[j], types[i]);
+                deck.add(card);
+            }
+        }
+        System.out.println("BUILD DECK");
+        System.out.println(deck);
+    }
+
+    public void shuffleDeck(){
+        for(int i = 0; i < deck.size(); i++){
+            int j = random.nextInt(deck.size());
+            Card currCard = deck.get(i);
+            Card randomCard = deck.get(j);
+            deck.set(i, randomCard);
+            deck.set(j, currCard);
+
+        }
+        System.out.println("AFTER SHUFFLE");
+        System.out.println(deck);
+    }
 }
