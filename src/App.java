9901ec0134fbdf490e1a527cd34083193ea7aebf
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-11-21 15:49:02 -0500

    created a new file for BrianJack

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         BlackJack BlackJack = new BlackJack();
+        BlackJack BrianJack = new BlackJack();
     }
 }
