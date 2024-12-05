4b6ff6fe763946bbc5d82a7368a941d1a9b9a7fd
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-12-05 15:36:44 -0500

    added a bunch of comments to understand code

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BlackJack BlackJack = new BlackJack();
-        //BrianJack BrianJack = new BrianJack();
+        //BlackJack BlackJack = new BlackJack();
+        BrianJack BrianJack = new BrianJack();
     }
 }
--- src/BrianJack.java
+++ src/BrianJack.java
@@ -1,14 +1,20 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.util.ArrayList;
-import java.util.Random;
+//NOTE: all of the imports are basically pre-made classes that you give a new name to in order to use them as objects in you own code(They are also known as Toolkits)
+//awt & swing are both ways to make a window but awt is older and more reliable whil swing is newer and has more functions 
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.BorderLayout; //This allows for the other functions to be moved around and in a LAYOUT
+import java.awt.Color; //This decides the color of what the background will be 
+import java.awt.Graphics; //Allows for things to be drawn on the window (i.e the cards)
+import java.awt.Image; //This is how you get images in java, for each image you wnat you have to start with this then the path to the image 
+
+//random classes that can make coding smoother such as pre-made arrays and randomizers
+import java.util.ArrayList; //creates an array based on name given to which can use .add or other modifers to check what is in array
+import java.util.Random; //when something passes into random they will then get a random number 
+
+//These are smaller functions and actions that can be taken on the window created by awt
+import javax.swing.ImageIcon; //normally images dont do nothing but with this, it allows the image to be clickable and be used as a button
+import javax.swing.JButton; //This creates a standard java button with like no styling
+import javax.swing.JFrame; //This creates the window that the game is created in
+import javax.swing.JPanel; //bascially like BorderLayout
 import javax.swing.border.Border;
 
 public class BrianJack {
@@ -17,21 +23,21 @@ public class BrianJack {
         String value; 
         String type; 
 
-        Card(String value, String type){
+        Card(String value, String type){ //This creates the card by giving a type and value
             this.value = value;
             this.type = type;
         }
 
-        public String toString(){
+        public String toString(){ //This turns it into a string so that it can be referred by getImagepath
             return value + "-" + type;
         }
 
         public int getValue(){
-            if("AJQK".contains(value)){ // A J O K
+            if("AJQK".contains(value)){ // A J Q K
                 if(value == "A"){
-                    return 11;
+                    return 11; //if its an Ace it will have a value of 11
                 }
-                return 10;
+                return 10; // otherwise it would be a face card so a 10
             }
             return Integer.parseInt(value); //2-10
         }
@@ -40,14 +46,18 @@ public class BrianJack {
         public boolean isAce(){
             return value == "A";
         }
+
+        public String getImagepath(){
+            return "./cards/" + toString() + ".png";
+        }
     }
 
-    ArrayList<Card> deck;
+    ArrayList<Card> deck; //making a list named deck and that list is made up of cards
     Random random = new Random(); //shuffling the deck 
 
 
     //dealer 
-    Card hiddenCard;
+    Card hiddenCard; 
     ArrayList<Card> dealerHand;
     int dealerSum; 
     int dealerAceCount;
@@ -65,17 +75,25 @@ public class BrianJack {
     int cardHeight = 154; //changable ration should be 1:1.4
 
     JFrame frame = new JFrame("Black Jack");
+    //This creates the gamePanel and all of the things rendered in it
     JPanel gamePanel = new JPanel() {
+        //When you start the game and load in these are the things that are loaded in first, instead of a blank screen
         @Override
         public void paintComponent(Graphics g){
             super.paintComponent(g);
 
             try {
+                //Creates the image of the card facedown
                 Image hiddenCardImg = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
                 g.drawImage(hiddenCardImg, 20, 20, cardWidth, cardHeight, null);
 
-
-
+                //This gives a value to the card that is face down and adding it to the dealersHand
+                for(int i = 0; i <dealerHand.size(); i++){
+                    Card card = dealerHand.get(i);
+                    Image cardImg = new ImageIcon(getClass().getResource(card.getImagepath())).getImage();
+                    g.drawImage(cardImg, cardWidth + 25 + (cardWidth + 5)*i, 20, cardWidth, cardHeight, null);
+                }
+                //This helps you track any bugs that might occur in the code
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -173,7 +191,7 @@ public class BrianJack {
 
     public void shuffleDeck(){
         for(int i = 0; i < deck.size(); i++){
-            int j = random.nextInt(deck.size());
+            int j = random.nextInt(deck.size()); //it is getting a random INT (bc of the next.Int) and having the upper limit be deck.size()
             Card currCard = deck.get(i);
             Card randomCard = deck.get(j);
             deck.set(i, randomCard);
